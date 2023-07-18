4a9faf470475129bae182bec3af57ff393af7b4d
krinalAspire <krinal.sonara@aspirespftserv.in>
2023-07-18 18:58:16 +0530

    18 july 2023, 6:58 pm

--- src/Invoice.jsx
+++ src/Invoice.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import { Button, createTheme, ThemeProvider, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
@@ -6,8 +6,17 @@ import Box from "@mui/material/Box";
 import InvoiceRecord from "./InvoicerRecord";
 import checkCircle from "./images/check-circle.svg";
 import trash from "./images/trash-2.svg";
+import AddIcon from "@mui/icons-material/Add";
 
 function Invoice() {
+  // const [fieldCount, setFieldCount] = useState(2); // Initial field count is 2 (for the first two InvoiceRecord components)
+
+  // const handleAddField = () => {
+  //   // Increment the field count when the user clicks "Add More Fields"
+  //   setFieldCount((prevCount) => prevCount + 1);
+  //   console.log(fieldCount);
+  // };
+
   const theme = createTheme({
     breakpoints: {
       values: {
@@ -19,6 +28,81 @@ function Invoice() {
       },
     },
   });
+
+  const customFont = {
+    xl: 14,
+    lg: 13,
+    md: 12,
+    sm: 11,
+    xs: 10,
+  };
+
+  const AddField = () => {
+    const [fieldCount, setFieldCount] = useState(2); // Initial field count is 2 (for the first two InvoiceRecord components)
+
+  const handleAddField = () => {
+    setFieldCount((prevCount) => prevCount + 1);
+    console.log(fieldCount);
+  };
+
+    return (
+      <>
+        {Array.from({ length: fieldCount - 2 }).map((_, index) => (
+          <InvoiceRecord
+            key={index}
+            fieldName=" Add Field"
+            fieldData="Add Data"
+          />
+        ))}
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ marginLeft: 4 }}>
+            <AddIcon
+              sx={{
+                color: "#868686",
+                width: {
+                  xl: "20px",
+                  lg: "18px",
+                  md: "17px",
+                  sm: "14px",
+                  xs: "14px",
+                },
+                height: {
+                  xl: "20px",
+                  lg: "18px",
+                  md: "17px",
+                  sm: "14px",
+                  xs: "14px",
+                },
+                marginRight: 1,
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                cursor: "pointer",
+                color: "#868686",
+                fontSize: customFont,
+                fontFamily: "Heebo",
+                fontWeight: 400,
+              }}
+              onClick={handleAddField}
+              // onClick={onAddField}
+            >
+              Add More Fields
+            </Typography>
+          </Box>
+        </Box>
+      </>
+    );
+  };
 
   return (
     <>
@@ -41,163 +125,225 @@ function Invoice() {
                 </Box>
               </Grid>
               <Grid item xs={3.5}>
-                <Box sx={{ border: "1px solid blue" }}>
+                <Box
+                  sx={{
+                    // borderLeft: "1px solid rgba(0, 0, 0, 0.06)",
+                    border: "1px solid blue",
+                    background: "#F7F7F7",
+                  }}
+                >
                   <Box
                     sx={{
-                      border: "1px solid red",
+                      // border: "1px solid red",
                       mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
                       my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
                     }}
                   >
-                    <Typography sx={{color:"rgba(30, 30, 30, 0.60)" }}>Invoice</Typography>
-                    <InvoiceRecord fieldName="Invoice No" fieldData="1325533"/>
-                    <InvoiceRecord fieldName="Invoice Date" fieldData="12/02/2023"/>
-                    <InvoiceRecord fieldName="Invoice Order Date" fieldData="10/02/2023"/>
-                    <InvoiceRecord fieldName="Invoice Order No." fieldData="SD-1245632"/>
-                  </Box>
-
-                  <Box
-                    sx={{
-                      border: "1px solid red",
-                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
-                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
-                    }}
-                  >
-                    <Typography sx={{color:"rgba(30, 30, 30, 0.60)" }}>Supplier</Typography>
-                    <InvoiceRecord fieldName="Supplier Name" fieldData="Supplier Address"/>
-                    <InvoiceRecord fieldName="Supplier Address" fieldData="2118 Thornridge Cir. Syracuse, Connecticut 35624"/>
-                  </Box>
-
-                  <Box
-                    sx={{
-                      border: "1px solid red",
-                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
-                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
-                    }}
-                  >
-                    <Typography sx={{color:"rgba(30, 30, 30, 0.60)" }}>Customer</Typography>
-                    <InvoiceRecord fieldName="Customer Name" fieldData="Cameron Williamson"/>
-                    <InvoiceRecord fieldName="Customer Address" fieldData="3891 Ranchview Dr. Richardson, California 62639"/>
-                  </Box>
-
-                  <Box
-                    sx={{
-                      border: "1px solid red",
-                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
-                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
-                    }}
-                  >
-                    <Typography sx={{color:"rgba(30, 30, 30, 0.60)" }}>Payment</Typography>
-                    <InvoiceRecord fieldName="Payment  Total" fieldData="9.460"/>
-                    <InvoiceRecord fieldName="Payment Total Value" fieldData="2.00"/>
-                  </Box>
-
-                  <Box
-                    sx={{
-                      border: "1px solid red",
-                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
-                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
-                    }}
-                  >
-                  <Grid container>
-                    <Grid item xs={10.5}>
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <Button
+                    <Typography
                       sx={{
-                        textTransform: "none",
-                        // fontSize: CustomFont,
-                        fontFamily: "Heebo",
-                        fontWeight: "500",
-                        borderRadius: "5px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: {
-                          xl: "421px",
-                          lg: "304px",
-                          md: "290px",
-                          sm: "263px",
-                          xs: "100px",
-                        },
-                        height: {
-                          xl: "54px",
-                          lg: "50px",
-                          md: "44px",
-                          sm: "40px",
-                          xs: "35px",
-                        },
-                        background: "#9F77EB",
-                        color: "white",
-                        //   my: { xl: 3, lg: 2.6, md: 2, sm: 1.5, xs: 1.3 },
-                        ":hover": {
-                          bgcolor: "#9F77EB",
-                          color: "white",
-                        },
-                        // top: { xl: "-12px", lg: "-10px", md: "-8px", sm: "-6px" },
+                        color: "rgba(30, 30, 30, 0.60)",
+                        fontSize: customFont,
+                        pl: { xl: 3, lg: 2, md: 2, sm: 1.65, xs: 1.65 },
                       }}
                     >
-                      <img src={checkCircle} alt="check-circle" style={{marginRight:"14px"}}/>
-                      Validate Document
-                    </Button>
-                    </Box>
-                    </Grid>
-                    <Grid item xs={1.5} align="right">
-                    {/* <Button
-                      sx={{
-                        textTransform: "none",
-                        // fontSize: CustomFont,
-                        fontFamily: "Heebo",
-                        fontWeight: "500",
-                        borderRadius: "5px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: {
-                          xl: "54px",
-                          lg: "15px",
-                          md: "10px",
-                          sm: "35px",
-                          xs: "25px",
-                        },
-                        height: {
-                          xl: "54px",
-                          lg: "50px",
-                          md: "40px",
-                          sm: "35px",
-                          xs: "25px",
-                        },
-                        background: "rgba(134, 134, 134, 0.20)",
-                        //   my: { xl: 3, lg: 2.6, md: 2, sm: 1.5, xs: 1.3 },
-                        ":hover": {
-                          bgcolor: "rgba(134, 134, 134, 0.20)",
-                        },
-                        // top: { xl: "-12px", lg: "-10px", md: "-8px", sm: "-6px" },
-                      }}
-                    >
-                      <img src={trash} alt="delete"/>
-                      {/* Validate Document */}
-                    {/* </Button>  */}
-                     <Box sx={{display:'flex', alignItems:'center', justifyContent:'center', background:"rgba(134, 134, 134, 0.20)",
-                      width: {
-                          xl: "54px",
-                          lg: "50px",
-                          md: "44px",
-                          sm: "40px",
-                          xs: "35px",
-                        },
-                        height: {
-                          xl: "54px",
-                          lg: "50px",
-                          md: "44px",
-                          sm: "40px",
-                          xs: "35px",
-                        }}}>
-                       <img src={trash} alt={"trash"} style={{display:'flex', alignItems:'center', justifyContent:'center'}} />
-                     </Box>
-                    </Grid>
-                  </Grid>
+                      Invoice
+                    </Typography>
+                    <InvoiceRecord fieldName="Invoice No" fieldData="1325533" />
+                    <InvoiceRecord
+                      fieldName="Invoice Date"
+                      fieldData="12/02/2023"
+                    />
+                    <InvoiceRecord
+                      fieldName="Invoice Order Date"
+                      fieldData="10/02/2023"
+                    />
+                    <InvoiceRecord
+                      fieldName="Invoice Order No."
+                      fieldData="SD-1245632"
+                    />
                   </Box>
 
+                  <Box
+                    sx={{
+                      // border: "1px solid red",
+                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
+                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        color: "rgba(30, 30, 30, 0.60)",
+                        fontSize: customFont,
+                        pl: { xl: 3, lg: 2, md: 2, sm: 1.65, xs: 1.65 },
+                      }}
+                    >
+                      Supplier
+                    </Typography>
+                    <InvoiceRecord
+                      fieldName="Supplier Name"
+                      fieldData="Supplier Address"
+                    />
+                    <InvoiceRecord
+                      fieldName="Supplier Address"
+                      fieldData="2118 Thornridge Cir. Syracuse, Connecticut 35624"
+                    />
+
+                    <AddField />
+                  </Box>
+
+                  <Box
+                    sx={{
+                      // border: "1px solid red",
+                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
+                      my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        color: "rgba(30, 30, 30, 0.60)",
+                        fontSize: customFont,
+                        pl: { xl: 3, lg: 2, md: 2, sm: 1.65, xs: 1.65 },
+                      }}
+                    >
+                      Customer
+                    </Typography>
+                    <InvoiceRecord
+                      fieldName="Customer Name"
+                      fieldData="Cameron Williamson"
+                    />
+                    <InvoiceRecord
+                      fieldName="Customer Address"
+                      fieldData="3891 Ranchview Dr. Richardson, California 62639"
+                    />
+
+                    <AddField />
+                  </Box>
+
+                  <Box
+                    sx={{
+                      // border: "1px solid red",
+                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
+                      mt: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
+                      mb:{xl:15, lg:13, md:11, sm:9, xs:7}
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        color: "rgba(30, 30, 30, 0.60)",
+                        fontSize: customFont,
+                        pl: { xl: 3, lg: 2, md: 2, sm: 1.65, xs: 1.65 },
+                      }}
+                    >
+                      Payment
+                    </Typography>
+                    <InvoiceRecord
+                      fieldName="Payment  Total"
+                      fieldData="9.460"
+                    />
+                    <InvoiceRecord
+                      fieldName="Payment Total Value"
+                      fieldData="2.00"
+                    />
+
+                    <AddField />
+                  </Box>
+
+                  <Box
+                    sx={{
+                      // border: "1px solid red",
+                      mx: { xl: 3.75, lg: 3, md: 2.75, sm: 2, xs: 1.75 },
+                      mb:{xl:3.6, lg:3.2, md:2.3, sm:2, xs:1.7}
+                      // my: { xl: 4.25, lg: 4, md: 3.25, sm: 3, xs: 2.25 },
+                    }}
+                  >
+                    <Grid container>
+                      <Grid item xs={10.5}>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                          <Button
+                            sx={{
+                              textTransform: "none",
+                              fontSize: {
+                                xl: 16,
+                                lg: 15,
+                                md: 12,
+                                sm: 10,
+                                xs: 10,
+                              },
+                              fontFamily: "Heebo",
+                              fontWeight: "500",
+                              borderRadius: "5px",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              width: {
+                                xl: "421px",
+                                lg: "304px",
+                                md: "290px",
+                                sm: "265px",
+                                xs: "245px",
+                              },
+                              height: {
+                                xl: "54px",
+                                lg: "50px",
+                                md: "44px",
+                                sm: "40px",
+                                xs: "35px",
+                              },
+                              background: "#9F77EB",
+                              color: "white",
+                              //   my: { xl: 3, lg: 2.6, md: 2, sm: 1.5, xs: 1.3 },
+                              ":hover": {
+                                bgcolor: "#9F77EB",
+                                color: "white",
+                              },
+                              // top: { xl: "-12px", lg: "-10px", md: "-8px", sm: "-6px" },
+                            }}
+                          >
+                            <img
+                              src={checkCircle}
+                              alt="check-circle"
+                              style={{ marginRight: "14px" }}
+                            />
+                            Validate Document
+                          </Button>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={1.5} align="right">
+                        <Box
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            background: "rgba(134, 134, 134, 0.20)",
+                            width: {
+                              xl: "54px",
+                              lg: "50px",
+                              md: "44px",
+                              sm: "40px",
+                              xs: "35px",
+                            },
+                            height: {
+                              xl: "54px",
+                              lg: "50px",
+                              md: "44px",
+                              sm: "40px",
+                              xs: "35px",
+                            },
+                          }}
+                        >
+                          <img
+                            src={trash}
+                            alt={"trash"}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                        </Box>
+                      </Grid>
+                    </Grid>
+                  </Box>
                 </Box>
               </Grid>
             </Grid>
--- src/InvoicerRecord.jsx
+++ src/InvoicerRecord.jsx
@@ -5,107 +5,192 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import { Typography } from "@mui/material";
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 
 const InvoiceRecord = (props) => {
-    const [field, setfield]=useState(props.fieldName)
-    const [data, setData] = useState(props.fieldData);
-    const [editMode, setEditMode] = useState(false);
-    const [editedData, setEditedData] = useState("");
-    const [editedfield, setEditedfield]=useState("");
-  
-    const handleEditClick = () => {
-      setEditMode(true);
-      setEditedData(data);
-      setEditedfield(field);
-    };
-  
-    const handleSaveClick = () => {
-      setData(editedData);
-      setfield(editedfield);
-      setEditMode(false);
-    };
-  
-    const handleCancelClick = () => {
-      setEditMode(false);
-    };
-  
-    const handleChange = (e) => {
-      setEditedData(e.target.textContent);
-      setEditedfield(e.target.textContent);
-    };
-  
-    return (
-      <>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            sx={{ background: editMode ? "rgba(159, 119, 235, 0.20)" : null, padding:{xl:1, md:0.5},  borderRadius:"5px" }}
-          >
-            <Grid item xs={1} sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-              {editMode ? (
-                <CheckIcon />
-              ) : (
-                <FiberManualRecordIcon
-                  sx={{
-                    color: "#868686",
-                    width: { md: "10px" },
-                    height: { md: "10px" },
-                  }}
-                />
-              )}
-            </Grid>
-            <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-              {editMode ? (
-                <Typography
-                  contentEditable
-                  value={editedfield}
-                  onBlur={handleSaveClick}
-                  onChange={handleChange}
-                  sx={{ border: "none", outline: "none", cursor:"pointer" }}
-                >
-                  {editedfield}
-                </Typography>
-              ) : (
-                <Typography onClick={handleEditClick} sx={{cursor:"pointer" }}>{field}</Typography>
-              )}
-            </Grid>
-            <Grid item xs={5} sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-              {/* Render additional data for the second column */}
-              {editMode ? (
-                <Typography
-                  contentEditable
-                  value={editedData}
-                  onBlur={handleSaveClick}
-                  onChange={handleChange}
-                  sx={{ border: "none", outline: "none", cursor:"pointer"  }}
-                >
-                  {editedData}
-                </Typography>
-              ) : (
-                <Typography onClick={handleEditClick} sx={{cursor:"pointer" }}>{data}</Typography>
-              )}
-            </Grid>
-            <Grid item xs={1} >
-              {/* Render Icons only in editMode */}
-              {editMode && (
-                <Box sx={{ display: "flex", justifyContent:'flex-end', alignItems:'center' }}>
-                  <Box>
-                    <CheckIcon onClick={handleSaveClick} sx={{cursor:"pointer" }}/>
-                  </Box>
-                  <Box>
-                    <CloseIcon onClick={handleCancelClick} sx={{cursor:"pointer" }}/>
-                  </Box>
-                </Box>
-              )}
-            </Grid>
-          </Grid>
-        </Box>
-      </>
-    );
+  const [field, setfield] = useState(props.fieldName);
+  const [data, setData] = useState(props.fieldData);
+  const [editMode, setEditMode] = useState(false);
+  const [editedData, setEditedData] = useState("");
+  const [editedfield, setEditedfield] = useState("");
+
+  const handleEditClick = () => {
+    setEditMode(true);
+    setEditedData(data);
+    setEditedfield(field);
   };
-  
-  export default InvoiceRecord;
-  
+
+  const handleSaveClick = () => {
+    setData(editedData);
+    setfield(editedfield);
+    setEditMode(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditMode(false);
+  };
+
+  // const handleChange = (e) => {
+  //   setEditedData(e.target.textContent);
+  //   setEditedfield(e.target.textContent);
+  // };
+
+  const handleChange = (e) => {
+    const content = e.target.textContent;
+    if (e.target === document.activeElement) {
+      // This is to handle contentEditable focus behavior
+      if (field !== editedfield) setEditedfield(content);
+      if (data !== editedData) setEditedData(content);
+    }
+  };
+
+  const CustomFontsize = {
+    xl: 16,
+    lg: 15,
+    md: 12,
+    sm: 10,
+    xs: 10,
+  };
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          sx={{
+            background: editMode ? "rgba(159, 119, 235, 0.20)" : null,
+            padding: { xl: 1, md: 0.5 },
+            borderRadius: "5px",
+          }}
+        >
+          <Grid
+            item
+            xs={1}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {editMode ? (
+              <CheckIcon />
+            ) : (
+              <FiberManualRecordIcon
+                sx={{
+                  color: "#868686",
+                  width: { md: "10px" },
+                  height: { md: "10px" },
+                }}
+              />
+            )}
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            }}
+          >
+            {editMode ? (
+              <Typography
+                contentEditable
+                value={editedfield}
+                onBlur={handleSaveClick}
+                onChange={handleChange}
+                sx={{
+                  border: "none",
+                  outline: "none",
+                  cursor: "pointer",
+                  fontFamily: "Heebo",
+                  fontWeight: 500,
+                  fontSize:CustomFontsize
+                }}
+              >
+                {editedfield}
+              </Typography>
+            ) : (
+              <Typography
+                onClick={handleEditClick}
+                sx={{ cursor: "pointer", fontFamily: "Heebo", fontWeight: 500, fontSize:CustomFontsize }}
+              >
+                {field}
+              </Typography>
+            )}
+          </Grid>
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+            }}
+          >
+            {/* Render additional data for the second column */}
+            {editMode ? (
+              <Typography
+                contentEditable
+                value={editedData}
+                onBlur={handleSaveClick}
+                onChange={handleChange}
+                sx={{
+                  border: "none",
+                  outline: "none",
+                  cursor: "pointer",
+                  fontFamily: "Heebo",
+                  fontWeight: 400,
+                  fontSize:CustomFontsize
+                }}
+              >
+                {editedData}
+              </Typography>
+            ) : (
+              <Typography
+                onClick={handleEditClick}
+                sx={{
+                  cursor: "pointer",
+                  color: "#868686",
+                  fontFamily: "Heebo",
+                  fontWeight: 400,
+                  fontSize:CustomFontsize
+                }}
+              >
+                {data}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={1}>
+            {/* Render Icons only in editMode */}
+            {editMode && (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  alignItems: "center",
+                }}
+              >
+                <Box>
+                  <CheckIcon
+                    onClick={handleSaveClick}
+                    sx={{ cursor: "pointer" }}
+                  />
+                </Box>
+                <Box>
+                  <CloseIcon
+                    onClick={handleCancelClick}
+                    sx={{ cursor: "pointer" }}
+                  />
+                </Box>
+              </Box>
+            )}
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default InvoiceRecord;
